e75deb2d78a3132d9c24613470f6d5fac2aee539
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-08-24 01:49:29 +0530

    added redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
+import { getPosts } from './actions/posts';
 import useStyles from './styles'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
@@ -10,7 +12,11 @@ const App = () => {
 
   const [currentId, setCurrentId] = useState(0);
   const classes = useStyles();
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch])
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
