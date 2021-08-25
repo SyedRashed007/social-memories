bcd3fb6e2a7363562b40c91c5a0048da09653683
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-08-26 02:36:52 +0530

    added changes

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
 
 import postRoutes from './routes/posts.js'
 
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -15,10 +18,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://Syed:12345@memory.qtxt6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on: ${PORT}`)))
     .catch((error) => console.log("Error while connecting", error.message))
 
